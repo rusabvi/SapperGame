3096d564447eb16c566fee3e756cf6c431f3f1f0
Руслан <rusabvi@gmail.com>
2022-09-08 14:08:11 +0600

    Консольное приложение готово

--- Sapper/Game.cs
+++ Sapper/Game.cs
@@ -4,12 +4,16 @@
     {
         private List<List<Cell>> _cells;
         private bool _over;
+        private bool _won;
         private int _openedCellAmount;
+        private int _bombAmount;
 
         public Game(int rowAmount, int columnAmount, int bombAmount)
         {
             _openedCellAmount = 0;
+            _bombAmount = bombAmount;
             _over = false;
+            _won = false;
             _cells = new List<List<Cell>>();
             for (int i = 0; i < rowAmount; i++)
             {
@@ -55,8 +59,10 @@
             var cellToCheck = _cells[row][column];
             if (cellToCheck.IsBombed())
                 _over = true;
+            if (!cellToCheck.IsOpened())
+                _openedCellAmount++;
             cellToCheck.Open();
-            _openedCellAmount++;
+            _won = _openedCellAmount >= _cells.Count * _cells[0].Count - 1 - _bombAmount;
         }
 
         private void CheckCells()
@@ -65,21 +71,26 @@
                 for (int j = 0; j < _cells[i].Count; j++)
                 {
                     var cellToCheck = _cells[i][j];
-                    for (int k = -1; k < 2; k++)
-                        for (int l = -1; l < 2; l++)
-                        {
-                            try
-                            {
-                                if (_cells[i + k][j + l].IsBombed() && !(k == 0 && l == 0))
-                                    cellToCheck.AddOneBombAround();
-                            }
-                            catch {; }
-                        }
+
+                    if (!cellToCheck.IsBombed())
+                        for (int k = -1; k < 2; k += 1)
+                            for (int l = -1; l < 2; l += 1)
+                                if (!(k == 0 && l == 0))
+                                {
+                                    try
+                                    {
+                                        var cellNear = _cells[i + k][j + l];
+                                        if (cellNear.IsBombed())
+                                            cellToCheck.AddOneBombAround();
+                                    }
+                                    catch { ; }
+                                }
                 }
         }
 
         public int GetOpenedCellAmount() => _openedCellAmount;
         public bool IsOver() => _over;
+        public bool IsWon() => _won;
         public List<List<Cell>> GetCells() => _cells;
     }
 }
--- SapperConsoleApp/Program.cs
+++ SapperConsoleApp/Program.cs
@@ -16,26 +16,44 @@ void DrawField(List<List<Cell>> cells)
     }
 }
 
-void SafePrint(ref int number, int min, int max)
+void SafeInput(ref int number, int min, int max)
 {
-    string numberString = Console.ReadLine();
-    while (!int.TryParse(numberString, out number) && number >= min && number <= max)
+    string? numberString;
+
+    var converted = false;
+
+    while (!converted)
     {
-        Console.Write($"Введите число от {min} до {max}: ");
         numberString = Console.ReadLine();
+        if (string.IsNullOrEmpty(numberString))
+            numberString = "";
+
+        try
+        {
+            number = int.Parse(numberString);
+
+            if (number >= min && number <= max)
+                converted = true;
+            else
+                throw new Exception();
+        }
+        catch
+        {
+            Console.Write($"Введите число от {min} до {max}: ");
+        }
     }
 }
 
 int rowAmount = new int(), columnAmount = new int(), bombAmount = new int();
 
 Console.Write("Введите количество рядов: ");
-SafePrint(ref rowAmount, 2, 30);
+SafeInput(ref rowAmount, 2, 30);
 
 Console.Write("Введите количество столбцов: ");
-SafePrint(ref columnAmount, 2, 30);
+SafeInput(ref columnAmount, 2, 30);
 
 Console.Write("Введите количество бомб: ");
-SafePrint(ref bombAmount, 1, rowAmount * columnAmount - 1);
+SafeInput(ref bombAmount, 1, rowAmount * columnAmount - 1);
 
 var game = new Game(rowAmount, columnAmount, bombAmount);
 
@@ -43,15 +61,15 @@ Console.WriteLine("Игра началась!");
 Thread.Sleep(2000);
 Console.Clear();
 
-while (!game.IsOver() || game.GetOpenedCellAmount() != rowAmount * columnAmount)
+while (!(game.IsOver() || game.IsWon()))
 {
     DrawField(game.GetCells());
     int row = new int(), column = new int();
     Console.WriteLine("Выберите ячейку");
     Console.Write("Ряд: ");
-    SafePrint(ref row, 1, rowAmount);
+    SafeInput(ref row, 1, rowAmount);
     Console.Write("Столбец: ");
-    SafePrint(ref column, 1, columnAmount);
+    SafeInput(ref column, 1, columnAmount);
     game.CheckCellIfItIsBombed(row - 1, column - 1);
     Console.Clear();
 }
